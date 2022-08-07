62f443caa4184e4a1cbb469cae815e86d6997c8a
Fərid Məmmədzadə <ferid.1235@mail.ru>
2022-08-07 23:13:58 +0400

    added routing class

---
+++ Simple_REST_API/Business/ApiRoutes.cs
@@ -0,0 +1,18 @@
+﻿namespace Simple_REST_API.Business
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+        
+        public static class Note
+        {
+            public const string GetAll = Base + "/note";
+            public const string Get = Base + "/note/{id:Guid}";
+            public const string Add = Base + "/note";
+            public const string Update = Base + "/note";
+            public const string Delete = Base + "/note/{id:Guid}";
+        }
+    }
+}
