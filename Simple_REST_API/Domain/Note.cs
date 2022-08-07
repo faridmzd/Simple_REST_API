d727e3e47c9a8e5b5433b30ce7394b78f7fd715d
Fərid Məmmədzadə <ferid.1235@mail.ru>
2022-08-07 22:47:42 +0400

    added note model

---
+++ Simple_REST_API/Domain/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Simple_REST_API.Domain
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+        
+    }
+}
