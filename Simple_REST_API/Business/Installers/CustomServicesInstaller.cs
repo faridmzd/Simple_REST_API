04b321506b2d67116fdfc242b9f999af20ce4af9
Fərid Məmmədzadə <ferid.1235@mail.ru>
2022-08-08 22:14:08 +0400

    created custom service installer for cleaner startup

---
+++ Simple_REST_API/Business/Installers/CustomServicesInstaller.cs
@@ -0,0 +1,18 @@
+﻿namespace Simple_REST_API.Business.Installers
+{
+    public static class CustomServicesInstaller
+    {
+
+        public static void InstallCustomServices(this IServiceCollection services)
+        {
+            InstallMvcServices(services);
+        }
+
+        private static void InstallMvcServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+            services.AddEndpointsApiExplorer();
+            services.AddControllers();
+        }
+    }
+}
