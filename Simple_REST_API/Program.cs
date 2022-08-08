c728f2fe73d2348c8637931e812d6906c9af0a99
Fərid Məmmədzadə <ferid.1235@mail.ru>
2022-08-08 22:14:24 +0400

    called custom services

--- Simple_REST_API/Program.cs
+++ Simple_REST_API/Program.cs
@@ -1,11 +1,11 @@
+using Simple_REST_API.Business.Installers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.InstallCustomServices();
 
 var app = builder.Build();
 
